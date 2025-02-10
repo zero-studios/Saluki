d4e1a76c809e9576747adeb3d40c4cc9db7d5efa
drake-zero <drake.nikolai@zero.nyc>
2025-02-10 15:56:29 -0600

    Add Vercel Image Component

---
+++ sveltekit-app/src/lib/components/molecules/VercelImage.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { optimize } from "$lib/vercel_image";
+
+    type Props = {
+        src: string;
+        alt: string;
+        loading: "lazy" | "eager";
+        aria_hidden: boolean | undefined;
+        classes: string;
+    };
+
+    let { src, alt, loading = "lazy", aria_hidden = undefined, classes } : Props = $props();
+
+    const src_set = optimize(src);
+</script>
+
+<img srcset="{src_set}" alt="{alt}" loading="{loading}" aria-hidden={aria_hidden ? "true" : undefined} class="{classes}" />
---
+++ sveltekit-app/src/lib/vercel_image.ts
@@ -0,0 +1,26 @@
+import { dev } from "$app/environment";
+
+export function optimize(
+	src: string,
+	widths = [200, 400, 800, 1200, 1600, 2000],
+	quality = 90,
+) {
+	if (dev) return src;
+
+	const optimizedSrcset = widths
+		.slice()
+		.sort((a, b) => a - b)
+		.map((width, i) => {
+			const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
+			const descriptor = i < widths.length - 1 ? ` ${width}w` : "";
+			return url + descriptor;
+		})
+		.join(", ");
+
+	return optimizedSrcset;
+}
+
+export function optimize_single(src: string, width = 800, quality = 90) {
+	if (dev) return src;
+	return `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
+}
