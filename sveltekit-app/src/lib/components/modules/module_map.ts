7c103f8eea5bdceeeba69b4a728e7e6d5a399eab
Nick Marino <nicholas.marino@zero.nyc>
2025-04-01 14:55:02 -0400

    Still encountering annoying naming bug

--- sveltekit-app/src/lib/components/modules/module_map.ts
+++ sveltekit-app/src/lib/components/modules/module_map.ts
@@ -1,5 +1,5 @@
-import Example from './Example.svelte';
 import type { Component } from 'svelte';
+import Example from "./Example.svelte"
 
 export const module_map: Record<string, Component> = {
     example: Example,
