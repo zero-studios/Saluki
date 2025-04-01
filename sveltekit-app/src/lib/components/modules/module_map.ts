2f2adde57911cf74326225394e2ff2c49fcda888
Nick Marino <nicholas.marino@zero.nyc>
2025-04-01 15:06:27 -0400

    Example route

--- sveltekit-app/src/lib/components/modules/module_map.ts
+++ sveltekit-app/src/lib/components/modules/module_map.ts
@@ -1,5 +1,6 @@
 import type { Component } from 'svelte';
-import Example from "./Example.svelte"
+import Example from "./Example.svelte";
+
 
 export const module_map: Record<string, Component> = {
     example: Example,
